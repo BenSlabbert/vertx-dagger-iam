c49cdc894a01a15900915417ed44ca224a55c119
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-06-20 17:27:01 +0200

    fix ui tests

--- ui/app/tests/test.js
+++ ui/app/tests/test.js
@@ -1,6 +1,16 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+test('home page has expected div with text home', async ({ page }) => {
+ await page.goto('/');
+ await expect(page.getByTestId('home-element')).toBeVisible();
+});
+
+test('login page has expected form', async ({ page }) => {
+	await page.goto('/login');
+	await expect(page.locator('form')).toBeVisible();
+});
+
+test('register page has expected form', async ({ page }) => {
+	await page.goto('/register');
+	await expect(page.locator('form')).toBeVisible();
 });
